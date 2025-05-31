20cb373b6134dc9d2a3d9bf137109e54f5dc4c80
vshivam05 <sv699291@gmail.com>
2025-05-31 21:16:55 +0530

    frontend started

--- backend/routes/transactionRoutes.js
+++ backend/routes/transactionRoutes.js
@@ -1,10 +1,14 @@
 import express from "express";
 import multer from "multer";
-import { uploadPDF } from "../controllers/transactionController.js";
+import {
+  uploadPDF,
+  searchTransactions,
+} from "../controllers/transactionController.js";
 
 const router = express.Router();
 import { upload } from "../middlewares/uploadMiddleware.js";
 
 router.post("/upload", upload.single("pdf"), uploadPDF);
+router.get("/transactions", searchTransactions);
 
 export default router;
