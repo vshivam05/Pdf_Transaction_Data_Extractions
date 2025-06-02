a5eb65bc9fde8de16f73bf36f06190340a3b4e05
vshivam05 <sv699291@gmail.com>
2025-06-02 17:53:59 +0530

    added previous db clear logic

--- frontend/src/API.jsx
+++ frontend/src/API.jsx
@@ -1,3 +1,2 @@
-export const API_URL = "http://localhost:5000";
-// export const API_URL =
-//   "https://pdf-transaction-data-extractions-2.onrender.com/api";
+// export const API_URL = "http://localhost:5000";
+export const API_URL = "https://pdf-transaction-data-extractions-2.onrender.com";
