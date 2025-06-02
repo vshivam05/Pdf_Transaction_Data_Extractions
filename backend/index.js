73b4a4294b45d8c0ac26d5321a9aa2a0cda384f0
vshivam05 <sv699291@gmail.com>
2025-06-02 10:59:31 +0530

    cors issue fixed

--- backend/index.js
+++ backend/index.js
@@ -16,12 +16,12 @@ app.use(express.json());
 
 
 app.use(cors({
-  origin: "http://localhost:5173",  // Match React dev server
+  origin: "*",
   methods: ["GET", "POST", "PUT", "DELETE"],
-  credentials: true  // Optional: if you're using cookies/sessions
 }));
 
-// Serve static files (PDFs) with correct headers
+
+
 const __filename = fileURLToPath(import.meta.url);
 const __dirname = path.dirname(__filename);
 
@@ -42,36 +42,3 @@ app.listen(PORT, () => {
   console.log(`Server is listening at port ${PORT}`);
 });
 
-
-// import express from "express";
-// import axios from "axios";
-// import dotenv from "dotenv"
-
-// dotenv.config();
-// const PORT = 3000;
-// const app = express();
-
-// app.use(express.json()); // Parses application/json requests
-
-// app.post("/translate", async (req, res) => {
-//   // const { q, source = "hi", target = "en" } = req.body;
-
-//   // if (!q) {
-//   //   return res.status(400).json({ error: "Missing 'q' in request body" });
-//   // }
-
-//   try {
-//     const response = await axios.post(
-//       `https://translation.googleapis.com/language/translate/v2?key=${process.env.API_KEY}&q=Hello World&target=hi`
-//     );
-
-//     res.json({ translatedText: response.data.data.translations });
-//   } catch (error) {
-//     console.error("Translation error:", error.response?.data || error.message);
-//     res.status(500).json({ error: "Failed to translate text" });
-//   }
-// });
-
-// app.listen(PORT, () => {
-//   console.log(`Server running at http://localhost:${PORT}`);
-// });
